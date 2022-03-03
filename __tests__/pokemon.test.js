6c789d0bc617ab958b02c4dc047a00f03b5dfc63
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-03-03 15:19:42 +0000

    reqorking the trainer catch method

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -1,92 +1,92 @@
-const { Pokemon, Trainer } = require('../pokemon.js');
-describe('Pokemon', () => {
-  test('returns an object', () => {
-    const testPokemon = new Pokemon('dog', 'dog', 'dog', 'dog');
-    expect(typeof testPokemon).toBe('object');
+const { Pokemon, Trainer, Battle } = require("../pokemon.js");
+describe("Pokemon", () => {
+  test("returns an object", () => {
+    const testPokemon = new Pokemon("dog", "dog", "dog", "dog");
+    expect(typeof testPokemon).toBe("object");
   });
-  test('default of type property is normal', () => {
+  test("default of type property is normal", () => {
     const testPokemon = new Pokemon();
-    expect(testPokemon.type).toBe('normal');
+    expect(testPokemon.type).toBe("normal");
   });
   test('type property can be set to "fire"', () => {
-    const testPokemon = new Pokemon('dog', 'fire');
-    expect(testPokemon.type).toBe('fire');
+    const testPokemon = new Pokemon("dog", "fire");
+    expect(testPokemon.type).toBe("fire");
   });
-  test('returns new Pokemon when given all arguments', () => {
+  test("returns new Pokemon when given all arguments", () => {
     const testPokemon = new Pokemon(
-      'Eevee',
-      'normal',
+      "Eevee",
+      "normal",
       55,
-      'Headbutt',
-      'Eev... Eevee!',
+      "Headbutt",
+      "Eev... Eevee!",
       18
     );
     expect(testPokemon).toEqual({
-      name: 'Eevee',
-      type: 'normal',
+      name: "Eevee",
+      type: "normal",
       hitpoints: 55,
-      move: 'Headbutt',
-      sound: 'Eev... Eevee!',
+      move: "Headbutt",
+      sound: "Eev... Eevee!",
       attackDamage: 18,
     });
   });
-  test('Strength prototype returns correct strength', () => {
-    const testPokemon = new Pokemon('dog', 'fire');
+  test("Strength prototype returns correct strength", () => {
+    const testPokemon = new Pokemon("dog", "fire");
     testPokemon.strength();
-    expect(testPokemon.strength).toBe('grass');
+    expect(testPokemon.strength).toBe("grass");
   });
-  test('weakness prototype returns correct weakness', () => {
-    const testPokemon = new Pokemon('dog', 'fire');
+  test("weakness prototype returns correct weakness", () => {
+    const testPokemon = new Pokemon("dog", "fire");
     testPokemon.weakness();
-    expect(testPokemon.weakness).toBe('water');
+    expect(testPokemon.weakness).toBe("water");
   });
-  test('talk method returns sound', () => {
+  test("talk method returns sound", () => {
     const testPokemon = new Pokemon(
-      'Eevee',
-      'normal',
+      "Eevee",
+      "normal",
       55,
-      'Headbutt',
-      'Eev... Eevee!',
+      "Headbutt",
+      "Eev... Eevee!",
       18
     );
-    expect(testPokemon.talk()).toEqual('Eev... Eevee!');
+    expect(testPokemon.talk()).toEqual("Eev... Eevee!");
   });
-  test('useYourMoves method returns move', () => {
+  test("useYourMoves method returns move", () => {
     const testPokemon = new Pokemon(
-      'Eevee',
-      'normal',
+      "Eevee",
+      "normal",
       55,
-      'Headbutt',
-      'Eev... Eevee!',
+      "Headbutt",
+      "Eev... Eevee!",
       18
     );
-    expect(testPokemon.useYourMoves()).toEqual('Headbutt');
+    expect(testPokemon.useYourMoves()).toEqual("Headbutt");
   });
 });
-describe('Trainer', () => {
-  test('catch method pushes pokemon into trainer', () => {
+describe.only("Trainer", () => {
+  test("catch method pushes pokemon into trainer", () => {
     const testPokemon = new Pokemon(
-      'Eevee',
-      'normal',
+      "Eevee",
+      "normal",
       55,
-      'Headbutt',
-      'Eev... Eevee!',
+      "Headbutt",
+      "Eev... Eevee!",
       18
     );
-    const testTrainer = new Trainer('Abdullah');
+    const testTrainer = new Trainer("Abdullah");
     testTrainer.catch(testPokemon);
     expect(testTrainer.pokemon).toEqual([
       {
-        name: 'Eevee',
-        type: 'normal',
+        name: "Eevee",
+        type: "normal",
         hitpoints: 55,
-        move: 'Headbutt',
-        sound: 'Eev... Eevee!',
+        move: "Headbutt",
+        sound: "Eev... Eevee!",
         attackDamage: 18,
       },
     ]);
   });
-  test('catch method only pushes when storage not full', () => {
+  test("catch method only pushes when storage not full", () => {
     const testPokemon = new Pokemon();
     const testPokemon2 = new Pokemon();
     const testPokemon3 = new Pokemon();
@@ -94,7 +94,7 @@ describe('Trainer', () => {
     const testPokemon5 = new Pokemon();
     const testPokemon6 = new Pokemon();
     const testPokemon7 = new Pokemon();
-    const testTrainer = new Trainer('Abdullah');
+    const testTrainer = new Trainer("Abdullah");
     testTrainer.catch(testPokemon);
     testTrainer.catch(testPokemon7);
     testTrainer.catch(testPokemon2);
@@ -104,4 +104,127 @@ describe('Trainer', () => {
     testTrainer.catch(testPokemon6);
     expect(testTrainer.pokemon.length).toBe(6);
   });
+});
+
+describe("Battle", () => {
+  test("returns an object", () => {
+    const newBattle = new Battle({ name: "Abdullah" });
+    expect(typeof newBattle).toBe("object");
+  });
+  test("Battle instance has correct properties", () => {
+    const testBattle = new Battle({ name: "Abdullah" });
+    expect(testBattle).toHaveProperty("trainer1");
+    expect(testBattle).toHaveProperty("trainer2");
+    expect(testBattle).toHaveProperty("trainer2Pokemon");
+    expect(testBattle).toHaveProperty("trainer1Pokemon");
+  });
+  test("Trainer instance can be added to a new Battle instance", () => {
+    const abdullahTrainer = new Trainer("Abdullah");
+    const testBattle = new Battle(abdullahTrainer);
+    expect(testBattle.trainer1).toEqual("Abdullah");
+  });
+  test("Trainer including pokemon instance can be added to a new Battle instance", () => {
+    const abdullahTrainer = new Trainer("Abdullah");
+    const eeveePokemon = new Pokemon(
+      "Eevee",
+      "normal",
+      55,
+      "Headbutt",
+      "Eev... Eevee!",
+      18
+    );
+
+    abdullahTrainer.catch(eeveePokemon);
+
+    const testBattle = new Battle(abdullahTrainer, "Eevee");
+    expect(testBattle.trainer1).toEqual("Abdullah");
+    expect(testBattle.trainer1Pokemon).toEqual({
+      name: "Eevee",
+      type: "normal",
+      hitpoints: 55,
+      move: "Headbutt",
+      sound: "Eev... Eevee!",
+      attackDamage: 18,
+    });
+  });
+
+  /*test("returns correct trainer when passed", () => {
+    const abdullahTrainer = new Trainer("Abdullah");
+    const testBattle = new Battle(abdullahTrainer);
+
+    expect
+  });
+
+  test("returns an object with correct trainer info", () => {
+    const abdullahTrainer = new Trainer("Abdullah");
+    const camTrainer = new Trainer("Cam");
+
+    const eeveePokemon = new Pokemon(
+      "Eevee",
+      "normal",
+      55,
+      "Headbutt",
+      "Eev... Eevee!",
+      18
+    );
+
+    const charmanderPokemon = new Pokemon(
+      "Charmander",
+      "fire",
+      44,
+      "flamethrower",
+      "Cha... Charmander!",
+      17
+    );
+
+    abdullahTrainer.catch({
+      name: "Eevee",
+      type: "normal",
+      hitpoints: 55,
+      move: "Headbutt",
+      sound: "Eev... Eevee!",
+      attackDamage: 18,
+    });
+    camTrainer.catch({
+      name: "Charmander",
+      type: "fire",
+      hitpoints: 44,
+      move: "flamethrower",
+      sound: "Cha... Charmander!",
+      attackDamage: 17,
+    });
+
+    const testBattle = new Battle(
+      abdullahTrainer,
+      abdullahTrainer.pokemon,
+      camTrainer,
+      camTrainer.pokemon
+    );
+
+    expect(testBattle).toEqual({
+      trainer1: abdullahTrainer,
+      trainer1Pokemon: [
+        {
+          name: "Eevee",
+          type: "normal",
+          hitpoints: 55,
+          move: "Headbutt",
+          sound: "Eev... Eevee!",
+          attackDamage: 18,
+        },
+      ],
+      trainer2: camTrainer,
+      trainer2pokemon: [
+        {
+          name: "Charmander",
+          type: "fire",
+          hitpoints: 44,
+          move: "flamethrower",
+          sound: "Cha... Charmander!",
+          attackDamage: 17,
+        },
+      ],
+    });
+  });
+  */
 });
--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-function Pokemon(name, type = 'normal', hitpoints, move, sound, attackDamage) {
+function Pokemon(name, type = "normal", hitpoints, move, sound, attackDamage) {
   this.name = name;
   this.type = type;
   this.hitpoints = hitpoints;
@@ -7,18 +7,18 @@ function Pokemon(name, type = 'normal', hitpoints, move, sound, attackDamage) {
   this.attackDamage = attackDamage;
 }
 Pokemon.prototype.strength = function () {
-  if (this.type === 'fire') {
-    this.strength = 'grass';
-  } else if (this.type === 'grass') {
-    this.strength = 'water';
-  } else this.strength = 'fire';
+  if (this.type === "fire") {
+    this.strength = "grass";
+  } else if (this.type === "grass") {
+    this.strength = "water";
+  } else this.strength = "fire";
 };
 Pokemon.prototype.weakness = function () {
-  if (this.type === 'grass') {
-    this.weakness = 'fire';
-  } else if (this.type === 'water') {
-    this.weakness = 'grass';
-  } else this.weakness = 'water';
+  if (this.type === "grass") {
+    this.weakness = "fire";
+  } else if (this.type === "water") {
+    this.weakness = "grass";
+  } else this.weakness = "water";
 };
 Pokemon.prototype.talk = function () {
   return this.sound;
@@ -28,10 +28,22 @@ Pokemon.prototype.useYourMoves = function () {
 };
 function Trainer(name) {
   this.name = name;
-  this.pokemon = [];
+  this.pokemon = {};
+  this.numberOfPokemon = 0;
 }
 Trainer.prototype.catch = function (pokemon) {
-  if (this.pokemon.length < 6) this.pokemon.push(pokemon);
-  else return 'Your Pokemon storage is full!';
+  if (this.numberOfPokemon < 6) {
+    this.pokemon[pokemon.name] = pokemon;
+    this.numberOfPokemon++;
+  } else return "Your Pokemon storage is full!";
 };
-module.exports = { Pokemon, Trainer };
+
+function Battle(trainer1, trainer1PokemonName, trainer2, trainer2PokemonName) {
+  this.trainer1 = trainer1.name;
+  this.trainer1Pokemon = trainer1.pokemon[trainer1PokemonName];
+  console.log(trainer1.pokemon[trainer1PokemonName]);
+  this.trainer2 = trainer2;
+  this.trainer2Pokemon = trainer2Pokemon;
+}
+
+module.exports = { Pokemon, Trainer, Battle };
